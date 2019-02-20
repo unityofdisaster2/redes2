e297e921c5067448d2c2f4cd33727cd5c057d2a5
Raymundo <rrosalesr1500@alumno.ipn.mx>
2019-02-19 20:02:11 -0600

    Se modifica codigo UDP para que servidor envie mensaje de eco al cliente

--- UDP/CEcoUDPB.java
+++ UDP/CEcoUDPB.java
@@ -9,7 +9,7 @@ import java.io.*;
 public class CEcoUDPB {
     public static void main(String[] args) {
         try {
-            DatagramSocket cl = new DatagramSocket();
+            DatagramSocket cl = new DatagramSocket(4000);
             System.out.println("Cliente iniciado...");
             BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
             String mensaje = br.readLine();
@@ -18,6 +18,11 @@ public class CEcoUDPB {
             int pto = 2000;
             DatagramPacket p = new DatagramPacket(b,b.length,InetAddress.getByName(dst),pto);
             cl.send(p);
+
+            DatagramPacket r = new DatagramPacket(new byte[10000],10000);
+            cl.receive(r);
+            String respuesta = new String(r.getData(),0,r.getLength());
+            System.out.println("respuesta: "+respuesta);
             cl.close();
         } catch (Exception e) {
             e.printStackTrace();
--- UDP/SEcoUDPB.java
+++ UDP/SEcoUDPB.java
@@ -9,11 +9,20 @@ public class SEcoUDPB{
             DatagramSocket s = new DatagramSocket(2000);
             System.out.println("Servidor iniciado");
             for(;;){
-                DatagramPacket p = new DatagramPacket(new byte[2000],2000);
+                //se crea variable para realizar lectura de datagrama
+                DatagramPacket p = new DatagramPacket(new byte[10000],10000);
+
                 s.receive(p); //bloqueo
                 System.out.println("Datagrama recibido desde "+p.getAddress()+":"+p.getPort());
                 String msj = new String(p.getData(),0,p.getLength());
                 System.out.println("Con el mensaje: "+msj);
+                System.out.println("se envia mensaje de vuelta...");
+                msj = "servidor: "+msj;
+                byte[] b = msj.getBytes();
+                //se crea variable para realizar envio de datagrama 
+                DatagramPacket w = new DatagramPacket(b,b.length,p.getAddress(),4000);
+                s.send(w);                
+
             }
         }catch(Exception e){
             e.printStackTrace();
