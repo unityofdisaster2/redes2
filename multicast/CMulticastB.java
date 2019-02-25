57f7fd878d70e80cc2d2a63c5ba7d7d170732cbf
Raymundo <rrosalesr1500@alumno.ipn.mx>
2019-02-24 20:36:13 -0600

    se modifica multicast para que el cliente pueda inyectar mensajes al canal

--- multicast/CMulticastB.java
+++ multicast/CMulticastB.java
@@ -12,6 +12,7 @@ public class CMulticastB {
 
     public static void main(String[] args) {
         InetAddress gpo = null;
+        int lock  = 0;
         try {
             MulticastSocket cl = new MulticastSocket(9999);
             System.out.println("Cliente escuchando puerto: " + cl.getLocalPort());
@@ -26,23 +27,31 @@ public class CMulticastB {
             //se une cliente al grupo definido anteriormente
             cl.joinGroup(gpo);
             System.out.println("Unido al grupo");
+            String cad_cliente = "";
+            byte[] b;
             for (;;) {
                 //en el ciclo se estaran recibiendo mensajes del grupo
                 DatagramPacket p = new DatagramPacket(new byte[10], 10);
                 cl.receive(p);
                 BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-                String cad_cliente = br.readLine();
-                byte[] b = cad_cliente.getBytes();
                 String msj = new String(p.getData());
                 System.out.println("Datagrama recibido: " + msj);
                 System.out.println("Servidor descubierto: " + p.getAddress() + ":" + p.getPort());
+                
+
+                if(lock == 0){
+                    cad_cliente = br.readLine();
+                    lock = 1;
+                }
+                b = cad_cliente.getBytes();
+
                 DatagramPacket p_cliente = new DatagramPacket(b,b.length, gpo,9999);
                 cl.send(p_cliente);
-                /*try {
+                try {
                     Thread.sleep(1000);
                 } catch (Exception e) {
                     e.printStackTrace();
-                }*/
+                }
             }
         } catch (Exception e) {
             e.printStackTrace();
